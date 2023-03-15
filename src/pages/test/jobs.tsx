7939bdf9553ecc4ed61e167060c25c490e6bcb6d
Ian Brandão <ian.brandao@avantgardebrasil.com.br>
2023-03-15 00:11:47 -0300

    Fix host problem

--- src/pages/test/jobs.tsx
+++ src/pages/test/jobs.tsx
@@ -264,7 +264,7 @@ export const getServerSideProps: GetServerSideProps<JobsProps> = async () => {
 
   try {
     const response = await axios.post(
-      "https://jobs-listing-psi.vercel.app/api/jobs",
+      process.env.NEXT_PUBLIC_URL + "api/jobs",
       payload
     );
     const jobs = response.data.jobs;
